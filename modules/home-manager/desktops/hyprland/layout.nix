2517204e996f292b6d8e9ae343a1c8a7d2838ba9
eminent <eminent@taom.dev>
2024-07-20 19:16:15 -0400

    test

--- modules/home-manager/desktops/hyprland/layout.nix
+++ modules/home-manager/desktops/hyprland/layout.nix
@@ -24,6 +24,26 @@
     decoration = [
       "rounding = 8"
       "active_opacity = 1.0"
+      "inactive_opacity = 1.0"
+      "shadow_range = 4"
+      "shadow_render_power = 2"
+      "col.shadow = 0X66000000"
+      "fullscreen_opacity = 0.90"
+      "blur.enabled = true"
+      "blur.size = 1"
+      "blur.passes = 4"
+      "blur.ignore_opacity = true"
+      "blur.optimizations = true"
     ]
-  }
+    master = [
+      "no_gaps_when_only = false"
+      "inherit_fullscreen = false"
+    ]
+    gestures = [
+      "workspace_swipe = false"
+    ]
+    input = [
+      "kb_layout = us"
+    ]
+  };
 }
